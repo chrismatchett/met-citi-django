de9e41507041b03a52b2e4b1a731c245e008f452
Chris Matchett <cmatchett@belfastmet.ac.uk>
2025-01-27 19:58:01 +0000

    models up next

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+
+# Replace the existing home function with the one below
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
