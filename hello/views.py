c8654e2551f1740ce7fffbd685f158317bbad83d
Chris Matchett <cmatchett@belfastmet.ac.uk>
2025-02-11 16:32:08 +0000

    adding models

--- hello/views.py
+++ hello/views.py
@@ -5,9 +5,21 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 
-# Replace the existing home function with the one below
-def home(request):
-    return render(request, "hello/home.html")
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+
+from django.views.generic import ListView
+
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
 
 def about(request):
     return render(request, "hello/about.html")
@@ -26,4 +38,23 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+# Add these to existing imports at the top of the file:
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+
+# Add this code elsewhere in the file:
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+
 
