16fc90066af215a41377d6d1bb0d58bbbba8b5ce
Chris <cmatchett@belfastmet.ac.uk>
2025-03-03 13:58:07 +0000

    removed some redundancy from views

--- hello/views.py
+++ hello/views.py
@@ -1,25 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-import re
-from django.utils.timezone import datetime
+from django.shortcuts import render, redirect
+from django.utils.timezone import now
 from django.http import HttpResponse
-
-from django.shortcuts import redirect
+from django.views.generic import ListView
 from hello.forms import LogMessageForm
 from hello.models import LogMessage
-
-from django.views.generic import ListView
-
 
 class HomeListView(ListView):
     """Renders the home page, with a list of all messages."""
     model = LogMessage
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeListView, self).get_context_data(**kwargs)
-        return context
-
+    template_name = "hello/home.html"  # Explicitly specify the template
 
 def about(request):
     return render(request, "hello/about.html")
@@ -27,34 +16,22 @@ def about(request):
 def contact(request):
     return render(request, "hello/contact.html")
 
-
 def hello_there(request, name):
-    print(request.build_absolute_uri()) #optional
+    print(request.build_absolute_uri())
     return render(
         request,
-        'hello/hello_there.html',
+        "hello/hello_there.html",
         {
-            'name': name,
-            'date': datetime.now()
+            "name": name,
+            "date": now()
         }
     )
 
-# Add these to existing imports at the top of the file:
-from django.shortcuts import redirect
-from hello.forms import LogMessageForm
-from hello.models import LogMessage
-
-# Add this code elsewhere in the file:
 def log_message(request):
     form = LogMessageForm(request.POST or None)
-
-    if request.method == "POST":
-        if form.is_valid():
-            message = form.save(commit=False)
-            message.log_date = datetime.now()
-            message.save()
-            return redirect("home")
-    else:
-        return render(request, "hello/log_message.html", {"form": form})
-
-
+    if request.method == "POST" and form.is_valid():
+        message = form.save(commit=False)
+        message.log_date = now()
+        message.save()
+        return redirect("home")
+    return render(request, "hello/log_message.html", {"form": form})
